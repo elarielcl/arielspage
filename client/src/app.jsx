d933b9971c1f06366614f36631bf0606d0b4dd3e
Ariel Cáceres <el.ariel.cl@gmail.com>
2017-07-23 16:49:24 -0400

    Progress on React Components

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import HomePage from './components/HomePage.jsx';
+import { getMuiTheme, MuiThemeProvider} from 'material-ui/styles';
 
-ReactDom.render(<h1>Hello from React</h1>, document.getElementById('react-app'));
+
+ReactDom.render(
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <HomePage/>
+    </MuiThemeProvider>
+    , document.getElementById('react-app'));
